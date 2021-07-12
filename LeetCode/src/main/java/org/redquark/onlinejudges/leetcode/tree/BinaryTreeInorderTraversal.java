9c51a3c155ba5c07057d4364645857927fe24ea1
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-07-12 18:05:35 +0530

    Added LC-0094 - Binary Tree Inorder Traversal

---
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,51 @@
+package org.redquark.onlinejudges.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author Anirudh Sharma
+ */
+public class BinaryTreeInorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // List to store the output
+        List<Integer> output = new ArrayList<>();
+        // Base case
+        if (root == null) {
+            return output;
+        }
+        // Stack to store the nodes of the tree
+        Stack<TreeNode> nodes = new Stack<>();
+        // Reference to the root node
+        TreeNode temp = root;
+        // Loop until the temp is null or the stack is empty
+        while (!nodes.isEmpty() || temp != null) {
+            // If temp is not null, then we will push it to the stack
+            // and move to the left of it
+            if (temp != null) {
+                nodes.push(temp);
+                temp = temp.left;
+            }
+            // If temp is null, then pop the node from the stack,
+            // add it to the output list and move to the right
+            else {
+                temp = nodes.pop();
+                output.add(temp.val);
+                temp = temp.right;
+            }
+        }
+        return output;
+    }
+
+    static class TreeNode {
+        final int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
---
+++ LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/tree/BinaryTreeInorderTraversalTest.java
@@ -0,0 +1,40 @@
+package org.redquark.onlinejudges.leetcode.tree;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.redquark.onlinejudges.leetcode.tree.BinaryTreeInorderTraversal.TreeNode;
+
+public class BinaryTreeInorderTraversalTest {
+
+    private final BinaryTreeInorderTraversal testObject = new BinaryTreeInorderTraversal();
+
+    @Test
+    public void testInorderTraversal() {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        List<Integer> expected = Arrays.asList(1, 3, 2);
+        assertEquals(expected, testObject.inorderTraversal(root));
+
+        assertEquals(Collections.emptyList(), testObject.inorderTraversal(null));
+
+        root = new TreeNode(1);
+        expected = Collections.singletonList(1);
+        assertEquals(expected, testObject.inorderTraversal(root));
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        expected = Arrays.asList(2, 1);
+        assertEquals(expected, testObject.inorderTraversal(root));
+
+        root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        expected = Arrays.asList(1, 2);
+        assertEquals(expected, testObject.inorderTraversal(root));
+    }
+}
