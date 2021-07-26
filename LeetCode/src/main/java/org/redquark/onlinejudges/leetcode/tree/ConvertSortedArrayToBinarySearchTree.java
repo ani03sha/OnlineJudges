83ccb133952f945b513fa836fadc8fb1bf1237c5
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-07-26 18:30:49 +0530

    Added LC-0108 - Convert Sorted Array To Binary Search Tree

---
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,41 @@
+package org.redquark.onlinejudges.leetcode.tree;
+
+/**
+ * @author Anirudh Sharma
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        // Special case
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        // Create the tree recursively
+        return createTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode createTree(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        // Find the middle element
+        int middle = start + (end - start) / 2;
+        // Create the root of the subtree
+        TreeNode root = new TreeNode(nums[middle]);
+        // Create the left and right subtrees
+        root.left = createTree(nums, start, middle - 1);
+        root.right = createTree(nums, middle + 1, end);
+        // Return the root
+        return root;
+    }
+
+    static class TreeNode {
+        final int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
---
+++ LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/tree/ConvertSortedArrayToBinarySearchTreeTest.java
@@ -0,0 +1,27 @@
+package org.redquark.onlinejudges.leetcode.tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.redquark.onlinejudges.leetcode.tree.ConvertSortedArrayToBinarySearchTree.TreeNode;
+
+public class ConvertSortedArrayToBinarySearchTreeTest {
+
+    private final ConvertSortedArrayToBinarySearchTree testObject = new ConvertSortedArrayToBinarySearchTree();
+
+    @Test
+    public void testSortedArrayToBST() {
+        int[] nums = new int[]{-10, -3, 0, 5, 9};
+        TreeNode expected = testObject.sortedArrayToBST(nums);
+        assertEquals(0, expected.val);
+        assertEquals(-10, expected.left.val);
+        assertEquals(-3, expected.left.right.val);
+        assertEquals(5, expected.right.val);
+        assertEquals(9, expected.right.right.val);
+
+        nums = new int[]{1, 3};
+        expected = testObject.sortedArrayToBST(nums);
+        assertEquals(1, expected.val);
+        assertEquals(3, expected.right.val);
+    }
+}
