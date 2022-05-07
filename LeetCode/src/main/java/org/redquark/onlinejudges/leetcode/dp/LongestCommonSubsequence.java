ae856c8055c9a35f3c5b596a0d92458c7733abea
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-05-07 23:21:36 +0530

    Updated LC-1143 - Longest Common Subsequence

--- LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/dp/LongestCommonSubsequence.java
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/dp/LongestCommonSubsequence.java
@@ -5,25 +5,73 @@ package org.redquark.onlinejudges.leetcode.dp;
  */
 public class LongestCommonSubsequence {
 
-    public int longestCommonSubsequence(String text1, String text2) {
-        // Lengths of the strings
+    /**
+     * Time - O(mn)
+     * Space - O(mn)
+     */
+    public int longestCommonSubsequenceOne(String text1, String text2) {
+        // Special cases
+        if (text1 == null || text1.isEmpty() || text2 == null || text2.isEmpty()) {
+            return 0;
+        }
+        // Lengths of two strings
         int m = text1.length();
         int n = text2.length();
-        // Lookup table to store the length of longest
-        // common subsequence ending at i-th and j-th
-        // characters of text1 and text2 respectively.
+        // Lookup table to store the longest common subsequence until
+        // the i-th index in text1 and j-th index in text2.
         int[][] lookup = new int[m + 1][n + 1];
-        // Populate the table
+        // Loop through the strings
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                // If the characters are same
+                // If both the characters are same, then we need to just
+                // add 1 to the value at i - 1, j - 1
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     lookup[i][j] = 1 + lookup[i - 1][j - 1];
-                } else {
+                }
+                // Else, we will take the maximum of previous row and previous
+                // column value
+                else {
                     lookup[i][j] = Math.max(lookup[i - 1][j], lookup[i][j - 1]);
                 }
             }
         }
         return lookup[m][n];
+    }
+
+    public int longestCommonSubsequenceTwo(String text1, String text2) {
+        if (text1 == null || text1.isEmpty() || text2 == null || text2.isEmpty()) {
+            return 0;
+        }
+        // Lengths of two strings
+        int m = text1.length();
+        int n = text2.length();
+        // Lookup table to store the longest common subsequence until
+        // the i-th index in text1 and j-th index in text2.
+        int[][] lookup = new int[2][n + 1];
+        // Index to switch between 0 and 1 - for previous row
+        int index = 0;
+        // Loop through the strings
+        for (int i = 0; i <= m; i++) {
+            // Index to switch between 0 and 1 - for previous row
+            index = i & 1;
+            for (int j = 0; j <= n; j++) {
+                // If any of the index is 0, then we have no matching
+                // character
+                if (i == 0 || j == 0) {
+                    lookup[index][j] = 0;
+                }
+                // If both the characters are same, then we will add 1 to the
+                // value from the value at indices i - 1 and j - 1
+                else if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    lookup[index][j] = 1 + lookup[1 - index][j - 1];
+                }
+                // If the current characters are not same, then we need maximum
+                // of the values in the previous row or column
+                else {
+                    lookup[index][j] = Math.max(lookup[1 - index][j], lookup[index][j - 1]);
+                }
+            }
+        }
+        return lookup[index][n];
     }
 }
--- LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/dp/LongestCommonSubsequenceTest.java
+++ LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/dp/LongestCommonSubsequenceTest.java
@@ -9,17 +9,32 @@ public class LongestCommonSubsequenceTest {
     private final LongestCommonSubsequence testObject = new LongestCommonSubsequence();
 
     @Test
-    public void testLongestCommonSubsequence() {
+    public void testLongestCommonSubsequenceOne() {
         String text1 = "abcde";
         String text2 = "ace";
-        assertEquals(3, testObject.longestCommonSubsequence(text1, text2));
+        assertEquals(3, testObject.longestCommonSubsequenceOne(text1, text2));
 
         text1 = "abc";
         text2 = "abc";
-        assertEquals(3, testObject.longestCommonSubsequence(text1, text2));
+        assertEquals(3, testObject.longestCommonSubsequenceOne(text1, text2));
 
         text1 = "abc";
         text2 = "def";
-        assertEquals(0, testObject.longestCommonSubsequence(text1, text2));
+        assertEquals(0, testObject.longestCommonSubsequenceOne(text1, text2));
+    }
+
+    @Test
+    public void testLongestCommonSubsequenceTwo() {
+        String text1 = "abcde";
+        String text2 = "ace";
+        assertEquals(3, testObject.longestCommonSubsequenceTwo(text1, text2));
+
+        text1 = "abc";
+        text2 = "abc";
+        assertEquals(3, testObject.longestCommonSubsequenceTwo(text1, text2));
+
+        text1 = "abc";
+        text2 = "def";
+        assertEquals(0, testObject.longestCommonSubsequenceTwo(text1, text2));
     }
 }
