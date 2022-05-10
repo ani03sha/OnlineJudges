09aaf879abd78ee7b09b35a43caeb699990fd8aa
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-05-10 19:54:15 +0530

    Added LC-0039 - Combination Sum

--- LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/backtracking/CombinationSum.java
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/backtracking/CombinationSum.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class CombinationSum {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // List to store th combinatons
+        // List to store th combinations
         List<List<Integer>> combinations = new ArrayList<>();
         // Special case
         if (candidates == null || candidates.length == 0) {
@@ -23,24 +23,24 @@ public class CombinationSum {
         return combinations;
     }
 
-    private void backtrack(int[] candidates, int target, int start, List<Integer> currentList, List<List<Integer>> combinations) {
+    private void backtrack(int[] candidates, int target, int index, List<Integer> current, List<List<Integer>> combinations) {
         // Special cases
         if (target < 0) {
             return;
         }
         if (target == 0) {
-            combinations.add(new ArrayList<>(currentList));
+            combinations.add(new ArrayList<>(current));
         }
         // Check every candidate from start to the end of the array
-        for (int i = start; i < candidates.length; i++) {
+        for (int i = index; i < candidates.length; i++) {
             // Add current number to the current list (choose)
-            currentList.add(candidates[i]);
+            current.add(candidates[i]);
             // Test if this number gives us valid result (check)
             // We are not incrementing i because duplicates are
             // allowed
-            backtrack(candidates, target - candidates[i], i, currentList, combinations);
+            backtrack(candidates, target - candidates[i], i, current, combinations);
             // Backtrack now (backtrack)
-            currentList.remove(currentList.size() - 1);
+            current.remove(current.size() - 1);
         }
     }
 }
