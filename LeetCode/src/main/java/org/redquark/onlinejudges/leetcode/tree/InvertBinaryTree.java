197c6a613e3f72671ff81e2714038d2c361167e1
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-09-07 20:38:49 +0530

    Updated LC-0226 - Invert Binary Tree

--- LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/tree/InvertBinaryTree.java
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/tree/InvertBinaryTree.java
@@ -2,22 +2,71 @@ package org.redquark.onlinejudges.leetcode.tree;
 
 import org.redquark.onlinejudges.leetcode.common.TreeNode;
 
-/**
- * @author Anirudh Sharma
- */
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InvertBinaryTree {
 
-    public TreeNode invertTree(TreeNode root) {
-        // Base case
+    /**
+     * Time complexity - O(n)
+     * Space complexity - O(n) - stack space
+     * n being the number of nodes in the tree
+     */
+    public TreeNode invertTreeRecursive(TreeNode root) {
+        // Special cases
         if (root == null) {
             return null;
         }
-        // Get the left and right subtrees of the current node
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-        // Swap the subtrees
-        root.right = left;
-        root.left = right;
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        // Get left and right children of the current node
+        TreeNode leftChild = root.left;
+        TreeNode rightChild = root.right;
+        // Swap the children's places
+        root.right = leftChild;
+        root.left = rightChild;
+        // Recurse for subtrees
+        invertTreeRecursive(root.left);
+        invertTreeRecursive(root.right);
+        return root;
+    }
+
+    /**
+     * Time complexity - O(n)
+     * Space complexity - O(n)
+     * n being the number of nodes in the tree
+     */
+    public TreeNode invertTreeIterative(TreeNode root) {
+        // Special cases
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        // Queue to perform BFS
+        Queue<TreeNode> nodes = new LinkedList<>();
+        // Add root node to the queue
+        nodes.add(root);
+        // Loop until the queue becomes empty
+        while (!nodes.isEmpty()) {
+            // Get the current noe
+            TreeNode current = nodes.poll();
+            // Get left and right children of the current node
+            TreeNode leftChild = current.left;
+            TreeNode rightChild = current.right;
+            // Swap the children's places
+            current.right = leftChild;
+            current.left = rightChild;
+            // Add left and right children to the queue for further processing
+            if (current.left != null) {
+                nodes.offer(current.left);
+            }
+            if (current.right != null) {
+                nodes.offer(current.right);
+            }
+        }
         return root;
     }
 }
--- LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/tree/InvertBinaryTreeTest.java
+++ LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/tree/InvertBinaryTreeTest.java
@@ -8,10 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class InvertBinaryTreeTest {
 
-    private final InvertBinaryTree testObject = new InvertBinaryTree();
+    private final InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
 
     @Test
-    public void testInvertTree() {
+    public void testInvertTreeRecursive() {
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
         root.right = new TreeNode(7);
@@ -19,7 +19,7 @@ public class InvertBinaryTreeTest {
         root.left.right = new TreeNode(3);
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(9);
-        TreeNode expected = testObject.invertTree(root);
+        TreeNode expected = invertBinaryTree.invertTreeRecursive(root);
         assertEquals(4, expected.val);
         assertEquals(7, expected.left.val);
         assertEquals(2, expected.right.val);
@@ -31,10 +31,40 @@ public class InvertBinaryTreeTest {
         root = new TreeNode(2);
         root.left = new TreeNode(1);
         root.right = new TreeNode(3);
-        expected = testObject.invertTree(root);
+        expected = invertBinaryTree.invertTreeRecursive(root);
+        assertEquals(2, expected.val);
         assertEquals(3, expected.left.val);
         assertEquals(1, expected.right.val);
 
-        assertNull(testObject.invertTree(null));
+        assertNull(invertBinaryTree.invertTreeRecursive(null));
+    }
+
+    @Test
+    public void testInvertTreeIterative() {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        TreeNode expected = invertBinaryTree.invertTreeIterative(root);
+        assertEquals(4, expected.val);
+        assertEquals(7, expected.left.val);
+        assertEquals(2, expected.right.val);
+        assertEquals(9, expected.left.left.val);
+        assertEquals(6, expected.left.right.val);
+        assertEquals(3, expected.right.left.val);
+        assertEquals(1, expected.right.right.val);
+
+        root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        expected = invertBinaryTree.invertTreeIterative(root);
+        assertEquals(2, expected.val);
+        assertEquals(3, expected.left.val);
+        assertEquals(1, expected.right.val);
+
+        assertNull(invertBinaryTree.invertTreeRecursive(null));
     }
 }
