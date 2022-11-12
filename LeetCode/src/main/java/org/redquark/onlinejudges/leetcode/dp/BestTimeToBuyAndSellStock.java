1bae0000d351d0a65b0eed58ee99233be0a5803d
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-11-12 22:44:47 +0530

    Updated LC-0121 - Best Time To Buy And Sell Stock

--- LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/dp/BestTimeToBuyAndSellStock.java
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/dp/BestTimeToBuyAndSellStock.java
@@ -6,19 +6,22 @@ package org.redquark.onlinejudges.leetcode.dp;
 public class BestTimeToBuyAndSellStock {
 
     public int maxProfit(int[] prices) {
-        // Special cases
-        if (prices == null || prices.length < 2) {
-            return Integer.MIN_VALUE;
+        // Special case
+        if (prices == null || prices.length == 0) {
+            return 0;
         }
-        // We will keep track of minimum stock price
-        // and maximum profit while we are examine
-        // each element of the array
+        // Maximum profit
+        int maximumProfit = 0;
+        // Minimum value of the stock
         int minimumPrice = prices[0];
-        int maximumProfit = Integer.MIN_VALUE;
-        // Loop through the array
+        // Loop through the remaining prices
         for (int i = 1; i < prices.length; i++) {
-            minimumPrice = Math.min(minimumPrice, prices[i]);
+            // Maximum profit will be calculated when will maximize
+            // the difference between the current price and minimum
+            // price
             maximumProfit = Math.max(maximumProfit, prices[i] - minimumPrice);
+            // Minimize the price if possible
+            minimumPrice = Math.min(minimumPrice, prices[i]);
         }
         return maximumProfit;
     }
