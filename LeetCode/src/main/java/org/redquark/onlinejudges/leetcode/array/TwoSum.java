6debff0cc87a1a19d05daac9a4781da7e93a620f
Anirudh Sharma <anirudh03sharma@gmail.com>
2022-11-13 00:40:16 +0530

    Updated LC-0001 - Two Sum

--- LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/array/TwoSum.java
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/array/TwoSum.java
@@ -9,26 +9,33 @@ import java.util.Map;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        // Array to store the indices
-        int[] indices = {-1, -1};
         // Special case
-        if (nums == null || nums.length == 0) {
-            return indices;
+        if (nums == null || nums.length < 2) {
+            throw new IllegalArgumentException("Input should have minimum two elements.");
         }
-        // Map to store the difference and the corresponding index
-        Map<Integer, Integer> differenceMap = new HashMap<>();
-        // Loop through the array
+        // Array to store the pair of element indices which satisfy
+        // the two sum condition
+        int[] pair = new int[]{-1, -1};
+        // Map to store the compliments of every element in the array
+        // with respect to the target. For e.g. for a valid pair a and
+        // b => b = target - a. The key in the map will be the compliment
+        // an value would be the index of the element
+        Map<Integer, Integer> compliments = new HashMap<>();
+        // Loop through every element of the array
         for (int i = 0; i < nums.length; i++) {
-            int difference = target - nums[i];
-            // Check if the difference already exists
-            if (differenceMap.containsKey(difference)) {
-                // Add the indices in the output array
-                indices[0] = differenceMap.get(difference);
-                indices[1] = i;
-                break;
+            // Compliment of current element w.r.t target
+            int compliment = target - nums[i];
+            // Check if this compliment is already present
+            // in the map of compliments
+            if (compliments.containsKey(compliment)) {
+                pair[0] = compliments.get(compliment);
+                pair[1] = i;
+                return pair;
+            } else {
+                // Add the current element to the map with its index
+                compliments.put(nums[i], i);
             }
-            differenceMap.put(nums[i], i);
         }
-        return indices;
+        return pair;
     }
 }
