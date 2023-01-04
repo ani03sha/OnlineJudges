b797a1ac6ae2aa7715afb1d2afa4ba2abcc98ca6
Anirudh Sharma <anirudh03sharma@gmail.com>
2023-01-04 19:52:35 +0530

    Added LC-0130 - Surrounded Regions

---
+++ LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/array/SurroundedRegionsTest.java
@@ -0,0 +1,27 @@
+package org.redquark.onlinejudges.leetcode.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SurroundedRegionsTest {
+
+    private final SurroundedRegions testObject = new SurroundedRegions();
+
+    @Test
+    public void testSolve() {
+        char[][] board = new char[][]{
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'X', 'X'}
+        };
+        char[][] expected = new char[][]{
+                {'X', 'X', 'X', 'X'},
+                {'X', 'X', 'X', 'X'},
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'X', 'X'}
+        };
+        assertArrayEquals(expected, testObject.solve(board));
+    }
+}
