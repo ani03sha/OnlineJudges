536044ca655ae22978dcf58d18bbd90b9497ea1e
Anirudh Sharma <anirudh03sharma@gmail.com>
2023-04-14 08:20:47 +0530

    Updated LC-0238 - Product Of Array Except Self

--- LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/array/ProductOfArrayExceptSelf.java
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/array/ProductOfArrayExceptSelf.java
@@ -2,31 +2,66 @@ package org.redquark.onlinejudges.leetcode.array;
 
 public class ProductOfArrayExceptSelf {
 
-    public int[] productExceptSelf(int[] nums) {
+    /**
+     * Space complexity - O(n)
+     */
+    public int[] productExceptSelfSolutionOne(int[] nums) {
         // Special case
         if (nums == null || nums.length == 0) {
             return nums;
         }
         // Length of the array
         int n = nums.length;
-        // Array to store the result
-        int[] products = new int[n];
-        // A variable to keep track of product so far
-        int temp = 1;
-        // Initialize the first element in the result array
-        products[0] = temp;
-        // Loop through the array from left to right
+        // Array to store the left products
+        int[] leftProducts = new int[n];
+        // Array to store right products
+        int[] rightProducts = new int[n];
+        // Since we don't have any element before first element and
+        // no element after last element, we assign 1 for them in
+        // left and right arrays
+        leftProducts[0] = 1;
+        rightProducts[n - 1] = 1;
+        // Traverse from left to right
         for (int i = 1; i < n; i++) {
-            temp *= nums[i - 1];
-            products[i] = temp;
+            leftProducts[i] = nums[i - 1] * leftProducts[i - 1];
         }
-        // Reset the temp
-        temp = 1;
-        // Loop through the array from right to left
+        // Traverse from right to left
+        for (int i = n - 2; i >= 0; i--) {
+            rightProducts[i] = nums[i + 1] * rightProducts[i + 1];
+        }
+        // Now for final output we will multiply both arrays
+        for (int i = 0; i < n; i++) {
+            leftProducts[i] *= rightProducts[i];
+        }
+        return leftProducts;
+    }
+
+    /**
+     * Space complexity - O(1)
+     */
+    public int[] productExceptSelfSolutionTwo(int[] nums) {
+        // Special case
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+        // Length of the array
+        int n = nums.length;
+        // Cumulative product
+        int cumulativeProduct = 1;
+        // Array to store the final output
+        int[] output = new int[n];
+        // Traverse from left to right
+        for (int i = 0; i < n; i++) {
+            output[i] = cumulativeProduct;
+            cumulativeProduct *= nums[i];
+        }
+        // Reset the cumulative product
+        cumulativeProduct = 1;
+        // Traverse from right to left
         for (int i = n - 1; i >= 0; i--) {
-            products[i] *= temp;
-            temp *= nums[i];
+            output[i] *= cumulativeProduct;
+            cumulativeProduct *= nums[i];
         }
-        return products;
+        return output;
     }
 }
--- LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/array/ProductOfArrayExceptSelfTest.java
+++ LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/array/ProductOfArrayExceptSelfTest.java
@@ -9,13 +9,24 @@ public class ProductOfArrayExceptSelfTest {
     private final ProductOfArrayExceptSelf testObject = new ProductOfArrayExceptSelf();
 
     @Test
-    public void testProductExceptSelf() {
+    public void testProductExceptSelfSolutionOne() {
         int[] nums = new int[]{1, 2, 3, 4};
         int[] expected = new int[]{24, 12, 8, 6};
-        assertArrayEquals(expected, testObject.productExceptSelf(nums));
+        assertArrayEquals(expected, testObject.productExceptSelfSolutionOne(nums));
 
         nums = new int[]{-1, 1, 0, -3, 3};
         expected = new int[]{0, 0, 9, 0, 0};
-        assertArrayEquals(expected, testObject.productExceptSelf(nums));
+        assertArrayEquals(expected, testObject.productExceptSelfSolutionOne(nums));
+    }
+
+    @Test
+    public void testProductExceptSelfSolutionTwo() {
+        int[] nums = new int[]{1, 2, 3, 4};
+        int[] expected = new int[]{24, 12, 8, 6};
+        assertArrayEquals(expected, testObject.productExceptSelfSolutionTwo(nums));
+
+        nums = new int[]{-1, 1, 0, -3, 3};
+        expected = new int[]{0, 0, 9, 0, 0};
+        assertArrayEquals(expected, testObject.productExceptSelfSolutionTwo(nums));
     }
 }
