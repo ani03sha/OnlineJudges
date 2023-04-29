87d137167a33afb196800d8a7fded37d549d163c
Anirudh Sharma <anirudh03sharma@gmail.com>
2023-04-29 21:19:35 +0530

    Added LC-1697 - Checking Existence Of Edge Length Limited Paths

---
+++ LeetCode/src/main/java/org/redquark/onlinejudges/leetcode/graph/CheckingExistenceOfEdgeLengthLimitedPaths.java
@@ -0,0 +1,76 @@
+package org.redquark.onlinejudges.leetcode.graph;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CheckingExistenceOfEdgeLengthLimitedPaths {
+
+    public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
+        // Special case
+        if (n <= 0 || edgeList == null || edgeList.length == 0 || queries == null || queries.length == 0) {
+            throw new IllegalArgumentException("Invalid input parameters!");
+        }
+        // Array to store the output
+        boolean[] output = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            queries[i] = new int[]{queries[i][0], queries[i][1], queries[i][2], i};
+        }
+        // Sort the arrays in increasing order
+        Arrays.sort(queries, Comparator.comparingInt(a -> a[2]));
+        Arrays.sort(edgeList, Comparator.comparingInt(a -> a[2]));
+        // Instance of unionFind
+        UnionFind unionFind = new UnionFind(n);
+        int index = 0;
+        for (int[] query : queries) {
+            int u = query[0];
+            int v = query[1];
+            int limit = query[2];
+            int qId = query[3];
+            while (index < edgeList.length && edgeList[index][2] < limit) {
+                unionFind.union(edgeList[index][0], edgeList[index][1]);
+                index++;
+            }
+            output[qId] = unionFind.connected(u, v);
+        }
+        return output;
+    }
+
+    static class UnionFind {
+        final int[] parent;
+        final int[] rank;
+
+        UnionFind(int n) {
+            this.parent = new int[n];
+            this.rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.parent[i] = i;
+            }
+        }
+
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        void union(int x, int y) {
+            int px = find(x);
+            int py = find(y);
+            if (px != py) {
+                if (rank[px] < rank[py]) {
+                    parent[px] = py;
+                } else if (rank[px] > rank[py]) {
+                    parent[py] = px;
+                } else {
+                    parent[py] = px;
+                    rank[px]++;
+                }
+            }
+        }
+
+        boolean connected(int x, int y) {
+            return find(x) == find(y);
+        }
+    }
+}
---
+++ LeetCode/src/test/java/org/redquark/onlinejudges/leetcode/graph/CheckingExistenceOfEdgeLengthLimitedPathsTest.java
@@ -0,0 +1,41 @@
+package org.redquark.onlinejudges.leetcode.graph;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CheckingExistenceOfEdgeLengthLimitedPathsTest {
+
+    private final CheckingExistenceOfEdgeLengthLimitedPaths testObject = new CheckingExistenceOfEdgeLengthLimitedPaths();
+
+    @Test
+    public void testDistanceLimitedPathsExist() {
+        int n = 3;
+        int[][] edgeList = new int[][]{
+                {0, 1, 2},
+                {1, 2, 4},
+                {2, 0, 8},
+                {1, 0, 16}
+        };
+        int[][] queries = new int[][]{
+                {0, 1, 2},
+                {0, 2, 5}
+        };
+        boolean[] expected = new boolean[]{false, true};
+        assertArrayEquals(expected, testObject.distanceLimitedPathsExist(n, edgeList, queries));
+
+        n = 5;
+        edgeList = new int[][]{
+                {0, 1, 10},
+                {1, 2, 5},
+                {2, 3, 9},
+                {3, 4, 13}
+        };
+        queries = new int[][]{
+                {0, 4, 14},
+                {1, 4, 13}
+        };
+        expected = new boolean[]{true, false};
+        assertArrayEquals(expected, testObject.distanceLimitedPathsExist(n, edgeList, queries));
+    }
+}
